87f8959ebb4ad29c1e84abcf869fbc9f96f5b001
Leo Laksmana <beol@laksmana.com>
2016-10-14 23:55:54 +0000

    Update main script

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,9 +1,10 @@
+'use strict';
+
 console.log('Loading function');
 
-exports.handler = function(event, context) {
-    console.log('value1 =', event.key1);
-    console.log('value2 =', event.key2);
-    console.log('value3 =', event.key3);
-    context.succeed(JSON.stringify(event));  // Echo back the first key value
-    // context.fail('Something went wrong');
+exports.handler = (event, context, callback) => {
+    var message = { "message": event };
+
+    console.log("Message: ", JSON.stringify(message, null, 2));
+    callback(null, "Function completed");
 };
