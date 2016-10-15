11e9dc99ebc0e1163dfcb1fa2741e75b7dce53ae
Leo Laksmana <beol@laksmana.com>
2016-10-15 00:08:57 +0000

    Fix Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,26 @@ var grunt = require('grunt');
 grunt.loadNpmTasks('grunt-aws-lambda');
 
 grunt.initConfig({
-   lambda_invoke: {
-      default: {
-         options: {
-            file_name: 'HelloWorld.js'
-         }
-      }
-   },
-   lambda_deploy: {
-      default: {
-         function: 'HelloWorld'
-      }
-   },
-   lambda_package: {
-      default: {
-   }
-   }
+    lambda_invoke: {
+        default: {
+            options: {
+                file_name: 'HelloWorld.js'
+            }
+        }
+    },
+    lambda_deploy: {
+        default: {
+            function: 'HelloWorld',
+            options: {
+                region: 'ap-southeast-1',
+                handler: 'HelloWorld.handler'
+            }
+        }
+    },
+    lambda_package: {
+        default: {
+        }
+    }
 });
 
 grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy'])
