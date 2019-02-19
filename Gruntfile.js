2aba6532750033a5a82a5232d5aea3fc72db814d
Leo Laksmana <beol@laksmana.com>
2019-02-20 02:44:49 +0800

    chore: Add Prettier and ESLint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,27 @@
-var grunt = require('grunt');
+const grunt = require('grunt');
+
 grunt.loadNpmTasks('grunt-aws-lambda');
 
 grunt.initConfig({
-    lambda_invoke: {
-        default: {
-            options: {
-                file_name: 'HelloWorld.js'
-            }
-        }
+  lambda_invoke: {
+    default: {
+      options: {
+        file_name: 'HelloWorld.js',
+      },
     },
-    lambda_deploy: {
-        default: {
-            function: 'HelloWorld',
-            options: {
-                region: 'ap-southeast-1',
-                handler: 'HelloWorld.handler'
-            }
-        }
+  },
+  lambda_deploy: {
+    default: {
+      function: 'HelloWorld',
+      options: {
+        region: 'ap-southeast-1',
+        handler: 'HelloWorld.handler',
+      },
     },
-    lambda_package: {
-        default: {
-        }
-    }
+  },
+  lambda_package: {
+    default: {},
+  },
 });
 
-grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy'])
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy']);
